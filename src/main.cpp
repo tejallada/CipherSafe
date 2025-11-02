07bc57a3e010f62a1657bd111311211a7d987b82
Tej Allada <tejallada@gmail.com>
2025-11-02 17:32:11 -0500

    set up clock and architecture for the population of the Data Structures

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
-
+#include <chrono> //timer
 int main(){
-    std::cout << "hello world!" << std::endl;
+
+    //Populate the 2 data structures (trie and HashMap)
+    std::cout << "Populating Data Structures..." << std::endl;
+    auto start = std::chrono::high_resolution_clock::now(); //starting clock
+
+
+    //Load Trie Data Structure
+
+
+    auto end = std::chrono::high_resolution_clock::now();//stoping clock
+    std::chrono::duration<double> duration = end - start;
+    std::cout << "Time taken: " << duration.count() << " seconds" << std::endl;
+
+    //
+
+
     return 0;
 }
