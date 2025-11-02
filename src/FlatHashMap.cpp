2ee16a52aa5b3a7f441d2cb4e79adb11e46cf030
NavajS <spnavaj@gmail.com>
2025-11-02 18:26:02 -0500

    Created function to resize hash table and helper insertor function

--- src/FlatHashMap.cpp
+++ src/FlatHashMap.cpp
@@ -29,7 +29,32 @@ class HashMap {
             return passwordHash(key) % tableSize;
         }
 
+        void resizeInsertion(string& key, int value) {
+            int index = hashingFunction(key);
+            list<Node>& bucket = table[index];
+
+            for(Node& node : bucket) {
+                if(node.key == key) {
+                    node.value = value;
+                    return;
+                }
+            }
+            bucket.emplace_back(key, value);
+            numElements++;
+        }
+
         void resizeTable() {
-            
+            int previousTableSize = tableSize;
+            vector<list<Node> > previousTable = move(table);
+            tableSize *= 2;
+            numElements = 0;
+            table.clear();
+            table.resize(tableSize);
+
+            for(int i = 0; i < previousTableSize; ++i) {
+                for(Node& node : previousTable[i]) {
+                    resizeInsertion(node.key, node.value);
+                }
+            }
         }
 };
