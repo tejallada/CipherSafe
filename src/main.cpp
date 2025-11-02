d0fa12e3e1916d7fdf2700ad8068c5595af14d59
Tej Allada <tejallada@gmail.com>
2025-11-02 17:36:45 -0500

    added HashMap loading timer

--- src/main.cpp
+++ src/main.cpp
@@ -6,15 +6,27 @@ int main(){
     std::cout << "Populating Data Structures..." << std::endl;
     auto start = std::chrono::high_resolution_clock::now(); //starting clock
 
-
+    for (int i = 0; i < 100000; i++) {
+        int val = 0;
+    }
     //Load Trie Data Structure
 
 
     auto end = std::chrono::high_resolution_clock::now();//stoping clock
     std::chrono::duration<double> duration = end - start;
-    std::cout << "Time taken: " << duration.count() << " seconds" << std::endl;
+    std::cout << "Time to load Trie: " << duration.count() << " seconds" << std::endl;
 
-    //
+
+    start = std::chrono::high_resolution_clock::now(); //starting clock
+
+
+    //Load HashMap Data Structure
+
+
+    end = std::chrono::high_resolution_clock::now();//stoping clock
+    duration = end - start;
+    std::cout << "Time to load HashMap: " << duration.count() << " seconds" << std::endl;
+
 
 
     return 0;
