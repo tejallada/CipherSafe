e5a20aa769d9e3f2740b0426fbf95865d1afee9b
Tej Allada <tejallada@gmail.com>
2025-11-03 14:37:30 -0500

    added basics of strenght checker implimentation

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,11 @@
 #include "FlatTrie.cpp"
 
 using namespace std;
-HashMap myHashMap;
+HashMap myHashMap; //Actual hashmap we are calling
+vector<string> passwords; //filler for Trie
+
 void LoadTrie() {
     ifstream file("Data/rockyou.txt");
-    vector<string> passwords;
     string line;
 
     if (!file.is_open()) {
@@ -22,27 +23,38 @@ void LoadTrie() {
     file.close();
 
     cout << "Loaded " << passwords.size() << " passwords." << endl;
-    cout << "First password: " << passwords[0] << endl;
 
 }
 
 void LoadHashMap(HashMap& dict) {
     ifstream file("Data/rockyou.txt");
-    vector<string> passwords;
     string line;
-
     if (!file.is_open()) {
         cerr << "Error: could not open file." << endl;
     }
-
     while (getline(file, line)) {
         if (!line.empty()) dict.Insert(line, 1);
     }
-
     file.close();
-
     cout << "Loaded " << dict.getNumElements() << " passwords." << endl;
-    cout << "First password: "  << endl;
+}
+
+void StrengthCheck(string& userPassword) {
+    string weakReason = "";
+    string strength = "Strong";
+    if (myHashMap.search(userPassword)) {
+
+        //check the trie after this
+
+        cout << "Password found." << endl;
+    } else if (userPassword.length() < 5) {
+        strength = "Weak";
+        weakReason += "Too short of a password\n";
+    }else {
+        cout << "you have a strong password" << endl;
+    }
+    cout << "Password Strength: " << strength << endl;
+    cout << "Reason:\n" << weakReason << endl;
 }
 
 void PrintMainMenu() {
@@ -85,7 +97,10 @@ int main(){
         if (menuSelection == "1") {
             cout << "Checking Password Strength" << endl;
         }else if (menuSelection == "2") {
-            //StrengthCheck(); //check the strength of the password and explain why
+            cout << "Enter password: " << endl;
+            string usrPass;
+            cin >> usrPass;
+            StrengthCheck(usrPass); //check the strength of the password and explain why
         }else if (menuSelection == "3") {
             //ask for password length
             //generate password
